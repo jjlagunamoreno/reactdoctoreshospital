9d97e89402e39e641e310ae6794e168d50d033a5
jjlagunamoreno <jjlagunamoreno@gmail.com>
2024-10-21 13:38:39 +0200

    llamada a componente hijo con id

--- src/Components/Doctores.js
+++ src/Components/Doctores.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Global from './Global';
-
+import Global from './Global'
+import DetallesDoctores from './DetallesDoctores'
 export default class Doctores extends Component {
     state = {
-        doctores: []
+        doctores: [],
+        idDoctor: -1
     }
     loadDoctores = () => {
         var idHospital = this.props.idhospital;
         var request = "api/doctores/doctoreshospital/" + idHospital;
         var url = Global.apiDoctores + request;
         axios.get(url).then(response => {
-            console.log("leyendo servicio doctores...")
+            console.log("Leyendo servicio doctores");
             this.setState({
-                doctores: response.data
+                doctores: response.data,
+                idDoctor: -1
             })
         })
     }
@@ -21,44 +23,53 @@ export default class Doctores extends Component {
         this.loadDoctores();
     }
     componentDidUpdate = (oldProps) => {
-        //NUNCA LLAMAREMOS A NADA SI NO TENEMOS UN IF
+        //NUNCA LLAMAREMOS A NADA SI NO TENEMOS AQUI UN IF
         if (this.props.idhospital != oldProps.idhospital) {
             this.loadDoctores();
         }
     }
+    mostrarDetalleDoctor = (idDoctor) => {
+        this.setState({
+            idDoctor: idDoctor
+        })
+    }
     render() {
         return (
             <div>
-                <h2>
-                    Doctores del hospital:
-                    <span style={{ color: "red" }}>{this.props.idhospital}</span>
+                <h2>Doctores del hospital:
+                    <span style={{ color: "red" }}>
+                        {this.props.idhospital}
+                    </span>
                 </h2>
                 <table className='table table-bordered'>
                     <thead>
                         <tr>
                             <th>Apellido</th>
-                            <th>Opciones</th>
-                            {/* <th>Especialidad</th>
-                            <th>Salario</th>
-                            <th>ID Hospital</th> */}
+                            <th>Detalles</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             this.state.doctores.map((doc, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{doc.apellido}</td>
-                                        <td><button>Detalles</button></td>
-                                        {/* <td>{doc.especialidad}</td>
-                                        <td>{doc.salario}</td>
-                                        <td>{doc.idHospital}</td> */}
-                                    </tr>
-                                )
+                                return (<tr key={index}>
+                                    <td>{doc.apellido}</td>
+                                    <td>
+                                        <button
+                                            onClick={() => {
+                                                this.mostrarDetalleDoctor(doc.idDoctor)
+                                            }}>
+                                            Detalles
+                                        </button>
+                                    </td>
+                                </tr>)
                             })
                         }
                     </tbody>
                 </table>
+                {
+                    this.state.idDoctor != -1 &&
+                    (<DetallesDoctores iddoctor={this.state.idDoctor} />)
+                }
             </div>
         )
     }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 import MenuHospitales from './MenuHospitales'
 import Doctores from './Doctores'
+import DetallesDoctores from './DetallesDoctores'
 import Home from './Home'
 
 export default class Router extends Component {
@@ -10,13 +11,21 @@ export default class Router extends Component {
             var { idhospital } = useParams();
             return <Doctores idhospital={idhospital} />
         }
+        // function DetallesDoctoresElement() {
+        //     var { iddoctor } = useParams();
+        //     return <DetallesDoctores iddoctor={iddoctor} />
+        // }
         return (
             <BrowserRouter>
                 <MenuHospitales />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    {/* ruta para llamada al componente doctores */}
                     <Route path="/doctores/:idhospital"
                         element={<DoctoresElement />} />
+                    {/* ruta para llamada al componente detalles doctores
+                    <Route path="/detallesdoctores/:iddoctor"
+                        element={<DetallesDoctoresElement />} /> */}
                 </Routes>
             </BrowserRouter >
         )
